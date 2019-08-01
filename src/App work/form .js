30db42520c65482934966fec147d9f86d17289d5
pratap403 <53479145+pratap403@users.noreply.github.com>
2019-08-01 14:05:33 +0530

    from with similar type

---
+++ src/App work/form .js
@@ -0,0 +1,52 @@
+import React from "react"
+
+class App extends React.Component
+{
+    constructor ()
+    {
+        super()
+        
+        this.state = {
+            firstName : "",
+            lastName : ""
+        }
+        
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    
+    handleChange (event)
+    {
+        const {name, value} = event.target
+        this.setState({[name] : value})
+    }
+    
+    
+    render ()
+    {
+        return(
+                <div>
+                    <input type="text"
+                        name = "firstName"
+                        value = {this.state.firstName}
+                        placeholder = "First Name"
+                        onChange = {this.handleChange}
+                    /> 
+            
+                    <input type="text"
+                        name = "lastName"
+                        value = {this.state.lastName}
+                        placeholder = "Last Name"
+                        onChange = {this.handleChange}
+                    />
+            
+                        <h1> {this.state.firstName} {this.state.lastName} </h1>
+                    
+                </div>
+        
+        )
+    }
+}
+
+
+export default App
